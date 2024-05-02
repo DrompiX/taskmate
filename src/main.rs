5e73e86dad2694bd532dfc4915677ea25016f0d4
Dima T <mrlocky98@gmail.com>
2024-05-02 23:24:37 +0300

    Learn how to toggle data through slint checkbox

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::rc::Rc;
 
-use slint::{Model, ModelRc, VecModel};
+use slint::Model;
 
 slint::include_modules!();
 
-fn print_task_list(task_list: &VecModel<Task>) {
+fn print_task_list(task_list: &slint::VecModel<Task>) {
     println!("Tasks: [");
     for task in task_list.iter() {
         println!("\tTask info: {:?}", task)
@@ -12,41 +12,46 @@ fn print_task_list(task_list: &VecModel<Task>) {
     println!("]")
 }
 
+struct PlaceholderRepo {
+    pub tasks: Rc<slint::VecModel<Task>>,
+}
+
+impl PlaceholderRepo {
+    pub fn new() -> Self {
+        let tasks = Rc::new(slint::VecModel::from(vec![
+            Task { id: "DAILY-1".into(), completed: false, description: "Task 1".into() },
+            Task { id: "DAILY-2".into(), completed: true, description: "Task 2".into() },
+            Task { id: "DAILY-3".into(), completed: false, description: "Task 3".into() },
+            Task { id: "DAILY-4".into(), completed: false, description: "Task 4".into() },
+        ]));
+        PlaceholderRepo { tasks }
+    }
+
+    pub fn get(&self, task_id: String) -> Option<(usize, Task)> {
+        self.tasks
+            .iter()
+            .enumerate()
+            .find(|(_, task)| task.id == task_id)
+    }
+}
+
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
-    let tasks : Rc<VecModel<Task>> = Rc::new(VecModel::from(vec![
-        Task {id: "DAILY-1".into(), completed: false, description: "Task 1".into()},
-        Task {id: "DAILY-2".into(), completed: true, description: "Task 2".into()},
-        Task {id: "DAILY-3".into(), completed: false, description: "Task 3".into()},
-        Task {id: "DAILY-4".into(), completed: false, description: "Task 4".into()},
-    ]));
-    let tasks_model_rc = ModelRc::from(tasks.clone());
-    print_task_list(&tasks);
+    let task_repo = PlaceholderRepo::new();
+    let tasks_model_rc = slint::ModelRc::from(task_repo.tasks.clone());
+    print_task_list(&task_repo.tasks);
     ui.set_task_list(tasks_model_rc);
 
-    ui.on_request_mark_task(
-        move |task_id| {
-            for mut task in tasks.iter() {
-                if task.id == task_id {
-                    println!("Task before change {:?}", task);
-                    task.completed = !task.completed;
-                    println!("Changed state for {:?}", task);
-                    // tasks.push(Task {id: "1".into(), completed: false, description: "heh".into()});
-                }
-            }
-            print_task_list(&tasks);
-        }
-    );
-
-    // ui.on_request_increase_value({
-    //     let ui_handle = ui.as_weak();
-    //     move || {
-    //         let ui = ui_handle.unwrap();
-    //         let new_counter = ui.get_counter() + 1;
-    //         ui.set_counter(new_counter);
-    //         ui.set_name("Dmitry".into());
-    //     }
-    // });
+    ui.on_request_mark_task(move |task_id| {
+        task_repo.get(task_id.into()).map(|(row, task_val)| {
+            println!("Task before change {:?}", task_val);
+            let mut new_task_val = task_val.clone();
+            new_task_val.completed = !new_task_val.completed;
+            println!("Changed state for {:?}", new_task_val);
+            task_repo.tasks.set_row_data(row, new_task_val);
+        });
+        print_task_list(&task_repo.tasks);
+    });
 
     ui.run()
 }
